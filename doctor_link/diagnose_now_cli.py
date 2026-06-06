4f693730ff57575b36881160297d2fb1f5eb8b3b
linkyang01 <lineyang@msn.com>
2026-06-06 15:50:14 +0800

    p8: complete minimal diagnose-now CLI

--- doctor_link/diagnose_now_cli.py
+++ doctor_link/diagnose_now_cli.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 from pathlib import Path
 
 import click
@@ -10,10 +9,4 @@ from doctor_link.core.collector import collect_into_package
 from doctor_link.core.package_builder import build_diagnostic_package, event_from_scan
 from doctor_link.core.scanner import scan_library
 from doctor_link.core.test_planner import generate_test_plan
-from doctor_link.core.user_assertion_manager import add_user_assertion
-from doctor_link.core.verification_runner import run_verification
-from doctor_link.core.web_server import build_web_view
-
-
-@main.command("diagnose-now")
-@click.argument("
+from doctor_link.core
