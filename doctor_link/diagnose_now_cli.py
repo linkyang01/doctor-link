b401ec04bde89cf8d62bf8ce3ee97049e9b894bd
linkyang01 <lineyang@msn.com>
2026-06-06 18:02:49 +0800

    Add minimal diagnose-now CLI

--- doctor_link/diagnose_now_cli.py
+++ doctor_link/diagnose_now_cli.py
@@ -9,5 +9,4 @@ from doctor_link.core.package_builder import build_diagnostic_package, event_fro
 from doctor_link.core.scanner import scan_library
 from doctor_link.core.test_planner import generate_test_plan
 from doctor_link.core.user_assertion_manager import add_user_assertion
-from doctor_link.core.verification_runner import run_verification
-from doctor
+from doctor_link.core.ver
