edac7886f2eb209eac5029f65527f6fe8fa99a20
linkyang01 <lineyang@msn.com>
2026-06-06 18:00:17 +0800

    p8: repair minimal diagnose-now CLI file

--- doctor_link/diagnose_now_cli.py
+++ doctor_link/diagnose_now_cli.py
@@ -5,8 +5,9 @@ from pathlib import Path
 import click
 
 from doctor_link.cli import main
-from doctor_link.core.collector import collect_into_package
 from doctor_link.core.package_builder import build_diagnostic_package, event_from_scan
 from doctor_link.core.scanner import scan_library
 from doctor_link.core.test_planner import generate_test_plan
-from doctor_link.core
+from doctor_link.core.user_assertion_manager import add_user_assertion
+from doctor_link.core.verification_runner import run_verification
+from doctor
